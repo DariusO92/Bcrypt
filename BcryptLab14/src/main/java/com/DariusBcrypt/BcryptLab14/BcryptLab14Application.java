4b341002683d5073121126302f851b4387d6bf70
darius owens <darius.owens.j@gmail.com>
2022-09-01 22:04:15 -0700

    inital commit plus SiteUser model

---
+++ BcryptLab14/src/main/java/com/DariusBcrypt/BcryptLab14/BcryptLab14Application.java
@@ -0,0 +1,13 @@
+package com.DariusBcrypt.BcryptLab14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcryptLab14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcryptLab14Application.class, args);
+	}
+
+}
---
+++ BcryptLab14/src/main/java/com/DariusBcrypt/BcryptLab14/SiteUser.java
@@ -0,0 +1,45 @@
+package com.DariusBcrypt.BcryptLab14;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class SiteUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    private String userName;
+    private String password;
+
+    public SiteUser(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
